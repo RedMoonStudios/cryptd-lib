dd7476bdd4f8544dfea545710e34b9e381d1ae74
aszlig <aszlig@redmoonstudios.org>
2012-02-28 17:01:46 +0100

    Lib.HTTP: Remove unnecessary functions and imports.

--- Cryptd/Lib/HTTP.hs
+++ Cryptd/Lib/HTTP.hs
@@ -3,56 +3,13 @@
 module Cryptd.Lib.HTTP (request) where
 
 import Data.List (isPrefixOf)
-import Data.Maybe (fromMaybe)
 import Data.Function (on)
 import Data.Conduit (runResourceT)
 import Data.Conduit.Lazy (lazyConsume)
 import Network.Wai (Request(..), Response(..), responseLBS)
-import Network.URI (URI(..), parseRelativeReference,
-                    relativeTo, parseAbsoluteURI)
-import Network.HTTP (simpleHTTP)
-import Network.HTTP.Headers (Header(..), HeaderName(..), hdrName, headerMap)
-import qualified Network.HTTP.Types as HT
-import qualified Network.HTTP.Base as H
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as LB
-import qualified Data.CaseInsensitive as CI
 import qualified Network.HTTP.Conduit as HC
-
--- | Convert a 'HeaderName' into the representation used by "HT".
-fromHeaderName :: HeaderName -> CI.CI HT.Ascii
-fromHeaderName (HdrCustom s) = CI.mk . B.pack $ s
-fromHeaderName hn =
-    CI.mk . B.pack . fst . head $ filter ff headerMap
-  where
-    ff = (== hn) . snd
-
--- | Convert a representation used by "HT" into a 'HeaderName'.
-toHeaderName :: CI.CI HT.Ascii -> HeaderName
-toHeaderName ascii =
-    getRes result
-  where
-    getRes r | null r = HdrCustom unpacked
-             | otherwise = snd $ head r
-    unpacked = B.unpack . CI.original $ ascii
-    result = filter ff headerMap
-    ff = (== unpacked) . fst
-
--- | Convert a list of 'Header's into "HT" headers.
-fromHeaders :: [Header] -> [HT.Header]
-fromHeaders =
-    map hdrConv
-  where
-    hdrConv (Header key value) =
-        (fromHeaderName key, B.pack value)
-
--- | Convert "HT" headers unto a list of 'Header's.
-toHeaders :: [HT.Header] -> [Header]
-toHeaders =
-    map hdrConv
-  where
-    hdrConv (key, value) =
-        Header (toHeaderName key) (B.unpack value)
 
 -- | Join URLs using the first one as a basepath.
 --
@@ -82,15 +39,6 @@ joinURL =
 
     trimL = dropWhile (== '/')
     trimR = reverse . trimL . reverse
-
--- | Filter out certain headers.
---
--- Currently this is just 'HdrHost'.
-blacklist :: [Header] -> [Header]
-blacklist = filter (mh . hdrName)
-  where
-    mh HdrHost = False
-    mh _ = True
 
 -- | Perform a HTTP request using 'Request' and a base URL.
 request :: String -- ^ The base URL
